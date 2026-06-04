9bccc96c63b2e3faf54fc43a2043809d5346daad
Апельханец Андрей <adapelkhanets@edu.hse.ru>
2026-06-05 00:06:26 +0300

    Добавлен класс DietaryRecipe

--- homework_2tp.py
+++ homework_2tp.py
@@ -131,3 +131,16 @@ class ShoppingList:
                 new_shopping_list._items.append(item)
                 
         return new_shopping_list
+    
+class DietaryRecipe(Recipe):
+    def __init__(self, title: str, diet_type: str, ingredients: list = None):
+        super().__init__(title, ingredients)
+        self.diet_type = diet_type
+
+    def scale(self, ratio: float):
+        scaled_recipe = super().scale(ratio)
+        return DietaryRecipe(scaled_recipe.title, self.diet_type, scaled_recipe.ingredients)
+
+    def __str__(self):
+        parent_str = super().__str__()
+        return f"[{self.diet_type}] {parent_str}"
